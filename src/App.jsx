6814a772cf141544304a6b08381d6df9372a5cad
Omegea Hunter <98022368+urbanjunglegoddess@users.noreply.github.com>
2024-09-30 22:52:59 +0000

    Changes to customize, not finished yet

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Omegea Hunter",
+  title: "Software Engineer, Data Analyst, and UX/UI Designer",
+  email: "omegeadhunter@gmail.com",
+  gitHub: "urbanjunglegoddess",
+  instagram: "urbanjunglegoddess",
+  linkedIn: "omegeahunter",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
